3961e2eb31de827e86a87d7518d73c7088159634
nmurray1984 <nathan.d.murray@gmail.com>
2016-08-25 15:24:57 -0500

    Comments from Nathan

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -9,10 +9,16 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
+    // NM - make sure this has a public modifier.  It won't cause issues now, but will in bigger projects when
+    // you try to call it from another assembly (the default is protected, which is only accessible in this assembly)
     class Program
     {
         static void Main(string[] args)
         {
+            // NM - so that you can test the worker well, make this two parts.  1) to instanciate the object, and
+            // 2) to start the worker.  Something like var worker = new Worker(); worker.Start();
+            // This makes it more readable and people who have never seen your code understand what is happening.
+            // In the future also, this make it easier to implement methods for Stop() or Pause(), if that's ever necessary
             new Worker();
         }
     }
