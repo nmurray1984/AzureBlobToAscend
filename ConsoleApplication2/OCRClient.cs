d7db4356727a78757efdbd8c5f5d5fd336fe1848
nmurray1984 <nathan.d.murray@gmail.com>
2016-08-25 15:26:51 -0500

    Comments from Nathan

--- ConsoleApplication2/OCRClient.cs
+++ ConsoleApplication2/OCRClient.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication2
     {
         public TaxDocument ProcessTaxDocument(TaxDocumentScan document)
         {
+            // NM - you may want to rename this variable to something more readable.  Typing is cheap whereas understanding
+            // a piece of code is expensive.  Something like 'taxDocument'.
             TaxDocument tdout = new TaxDocument();
             tdout.ImportantTaxNumber = 42;
             tdout.PropertyName = "Six Flags over Atlanta";
